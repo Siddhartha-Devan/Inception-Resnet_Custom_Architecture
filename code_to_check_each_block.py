f23289a138ebcb7d6995c898139c96f999bcda7a
Siddharth <siddu2003oct@gmail.com>
2024-09-03 13:52:16 +0530

    built inception block

--- code_to_check_each_block.py
+++ code_to_check_each_block.py
@@ -50,3 +50,29 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=5)
 # model.evaluate(x_test, y_test)
+
+
+
+
+# for the inception block
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+print("x_train", x_train.shape)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.InputLayer(( 28, 28, 1)),
+  InceptionBlock(n_filters=64),
+  tf.keras.layers.Conv2D(kernel_size= (3,3), filters = 32),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+# model.evaluate(x_test, y_test)
